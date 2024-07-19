2f1da99444d20de05a44a2e4305bc5440b4fe15e
Albionii <albii.java22@gmail.com>
2024-07-20 00:41:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import AddClient from './AddClient/AddClient';
 import ViewClient from './ViewClients/ViewClient';
 
 function App() {
   return (
-    <Router basename='/vka/'>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add" element={<AddClient />} />
